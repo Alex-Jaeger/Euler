1a50fb4ac948e313162703c728462a32ffea2167
Alex Jaeger <agjaeger@ualr.edu>
2014-10-24 00:16:07 -0500

    Completed - Names Scores

--- namesscores.py
+++ namesscores.py
@@ -1,11 +1,15 @@
 
-names_file = open("./data/namesscores_names.txt")
-names = [name.replace('"', '') for name in names_file.readlines()[0].split(',')]
+names = [name.replace('"', '') for name in open("./data/namesscores_names.txt").readlines()[0].split(',')]
 
 names = sorted(names)
-
 result = 0
-for i in range(0,len(names)):
-    result += sum([ord(character) for character in names[i]]) * (i + 1)
+
+for i in range(0, len(names)):
+    char_sum = 0
+
+    for character in names[i]:
+        char_sum += (ord(character) - ord('@'))
+
+    result += (char_sum * (i+1))
 
 print result
