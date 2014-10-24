0bfdc773173c9b354bc852b25926c65331504fe1
Alex Jaeger <agjaeger@ualr.edu>
2014-10-24 09:19:52 -0500

    Started - Number Letter Counts

---
+++ numberletter.py
@@ -0,0 +1,33 @@
+
+number_words = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "teen",
+    "eleven",
+    "tweleve",
+    "thirteen",
+    "fifteen",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+    "hundred",
+    "thousand"
+]
+
+result = 0
+
+for i in range(1, 1001):
+    
