794fe1a814b03385a3e324cf407203dee5e2d585
Alex Jaeger <agjaeger@ualr.edu>
2014-10-23 19:52:47 -0500

    Completed - Even Fibonacci Numbers

---
+++ evenfibo.py
@@ -0,0 +1,20 @@
+
+import math
+
+result = 0
+
+# Successor Formula
+i = 0
+j = 1
+
+while j < 4000000:
+
+    fibo_num = i + j
+
+    if fibo_num % 2 == 0:
+        result += fibo_num
+
+    i = j
+    j = fibo_num
+
+print result
