cdcfaae904c1530f2c1bbd040352619efa3e10fc
Alex Jaeger <agjaeger@ualr.edu>
2014-10-26 15:27:39 -0500

    Completed - Number Letter Counts

--- numberletter.py
+++ numberletter.py
@@ -1,33 +1,12 @@
 
-number_words = [
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "teen",
-    "eleven",
-    "tweleve",
-    "thirteen",
-    "fifteen",
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
-    "hundred",
-    "thousand"
-]
+from num2words import num2words
 
 result = 0
 
 for i in range(1, 1001):
-    
+    num_word = num2words(i)
+    num_word = num_word.replace(" ", "")
+    num_word = num_word.replace("-", "")
+    result += len(num_word)
+
+print result
