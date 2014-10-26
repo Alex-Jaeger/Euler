00635c4764b65c86845f42654d14e8e5f3e3d514
Alex Jaeger <agjaeger@ualr.edu>
2014-10-26 16:56:21 -0500

    Completed - Smallest Multiple

--- smallestmultiple.py
+++ smallestmultiple.py
@@ -1,12 +1,12 @@
 
 def checkMultiple(num):
     divisible = 0
-
-    for i in range(1, 21):
+    
+    for i in range(11, 21):
         if num % i == 0:
             divisible += 1
 
-    if divisible == 20:
+    if divisible == 10:
         return 1
     else:
         return 0
@@ -14,7 +14,6 @@ def checkMultiple(num):
 cur_num = 20
 
 while checkMultiple(cur_num) == 0:
-    print cur_num
     cur_num += 1
 
 print cur_num
